15f45c132e605bc065e9cb4b58e5f7a327ab0174
Jethro Estrada <jethroestrada237@gmail.com>
2026-04-28 15:52:42 +0800

    Updates

--- jethro/start-llama-server-llm-airm1.sh
+++ jethro/start-llama-server-llm-airm1.sh
@@ -20,6 +20,24 @@ llama-server \
 # JA - EN Translator
 llama-server \
   --model /Users/jethroestrada/.cache/llama.cpp/llm_models/mistralai_Ministral-3-3B-Instruct-2512-Q4_K_M.gguf \
+  --ctx-size 4096 \
+  --n-gpu-layers all \
+  --flash-attn on \
+  --batch-size 1024 \
+  --ubatch-size 512 \
+  --mlock \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --no-mmap \
+  --cont-batching \
+  --cache-type-k q8_0 \
+  --cache-type-v q8_0 \
+  --jinja
+
+
+# Don't use these settings, too slow
+llama-server \
+  --model /Users/jethroestrada/.cache/llama.cpp/llm_models/mistralai_Ministral-3-3B-Instruct-2512-Q4_K_M.gguf \
   --chat-template-file /Users/jethroestrada/Desktop/External_Projects/Jet_Windows_Workspace/jinja_templates/ministral-3b-instruct.jinja \
   --jinja \
   --ctx-size 4096 \
