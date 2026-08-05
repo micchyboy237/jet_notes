a0dae3a6d42c9796aa27ed3283ba0a98537ad16f
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-06 00:48:14 +0800

    Updates

--- jethro/update-python-libs.sh
+++ jethro/update-python-libs.sh
@@ -1,3 +1,4 @@
 TARGET_DIR="$HOME/Desktop/External_Projects/AI"
 echo "Starting Python library update in $TARGET_DIR ..."
 python "$HOME/Desktop/External_Projects/Jet_Projects/jet_notes/python_scripts/git_pull_all_repos.py" "$TARGET_DIR"
+source /Users/jethroestrada/Desktop/External_Projects/Jet_Projects/jet_notes/jethro/editable_packages/update-editable-packages.sh
