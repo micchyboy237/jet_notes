b33c3fd71a07a833b8b50170fd824f3e2126b964
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-05 23:07:35 +0800

    Updates

--- jethro/start-windows-llm-lenovo.ps1
+++ jethro/start-windows-llm-lenovo.ps1
@@ -3,4 +3,5 @@
 llama-server -m "C:\Users\Dreydeveloper\.cache\llama.cpp\llm\Qwen3.5-2B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf" `
   --mmproj "C:\Users\Dreydeveloper\.cache\llama.cpp\vision\mmproj-Qwen3.5-2B-Uncensored-HauhauCS-Aggressive-f16.gguf" `
   -ngl 99 -c 16384 -ctk q8_0 -ctv q8_0 --image-max-tokens 256 -t 6 `
-  --host 0.0.0.0 --port 8080 -fit off
+  --host 0.0.0.0 --port 8080 -fit off `
+  --chat-template-file "C:\Users\Dreydeveloper\Desktop\Jet_Files\AI_Files\llama_cpp\jinja-templates\Qwen3.5_2B_adjusted.jinja"
